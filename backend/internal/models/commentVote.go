16355ff1237dbf2d5ca87406baaef2782dde9d29
Desk4444 <desk4444@tutamail.com>
2026-08-24 21:04:13 +0200

    backend models changes

--- backend/internal/models/commentVote.go
+++ backend/internal/models/commentVote.go
@@ -1,15 +1,20 @@
 package models
 
-
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type CommentVote struct {
-	gorm.Model
-
-	UserID    uint64 `json:"userID"`
-	CommentID uint64 `json:"commentID"`
-
-	Vote int `json:"vote"`
+	ID        uuid.UUID      `gorm:"primary_key;type:uuid;default:gen_random_uuid()" json:"id"`
+	UserID    uuid.UUID      `gorm:"type:uuid; constraint:onDelete:CASCADE" json:"user_id"`
+	CommentID uuid.UUID      `gorm:"type:uuid; uniqueIndex; constraint:onDelete:CASCADE" json:"comment_id"`
+	Comment   Comment        `gorm:"foreignKey:CommentID; references:ID" json:"-"`
+	User      User           `gorm:"foreignKey:UserID; references:ID" json:"-"`
+	Vote      int            `gorm:"type:int" json:"vote"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
