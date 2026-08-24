a180b13961690c9900829060d4f4226b625624a2
Desk4444 <desk4444@tutamail.com>
2026-08-24 21:04:13 +0200

    backend models changes

--- backend/internal/models/comment.go
+++ backend/internal/models/comment.go
@@ -1,25 +1,24 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Comment struct {
-	gorm.Model
-
-	Comment      string    `gorm:"type:text" json:"comment"`
-	CommentTitle string    `json:"comment_title"`
-	Rating       float64   `json:"rating"`
-	Likes        int       `gorm:"default:0" json:"likes"`
-	Dislikes     int       `gorm:"default:0" json:"dislikes"`
-	UserID       uuid.UUID `json:"user_id"`
-	GameID       uint64    `json:"game_id"`
-	Author       string	   `json:"author"`
-	User         User      `gorm:"foreignKey:UserID;references:ID"`
-	Title1		 string	   `json:"title_1"`
-	Title2		 string	   `json:"title_2"`
-	Game         Game      `gorm:"foreignKey:GameID;references:AppID"`
-	ProfilePic   string    `gorm:"typevarchar(100)" json:"profile_picture"`
-	
+	ID           uuid.UUID      `gorm:"primary_key;type:uuid;default:gen_random_uuid()" json:"id"`
+	Comment      string         `gorm:"type:text" json:"comment"`
+	CommentTitle string         `gorm:"type:text" json:"comment_title"`
+	Rating       float64        `gorm:"type:float" json:"rating"`
+	Likes        int            `gorm:"type:int; default:0" json:"likes"`
+	Dislikes     int            `gorm:"type:int; default:0" json:"dislikes"`
+	UserID       uuid.UUID      `gorm:"type:uuid; constraint:onDelete:CASCADE" json:"user_id"`
+	GameID       uint64         `gorm:"type:bigint" json:"game_id"`
+	User         User           `gorm:"foreignKey:UserID;references:ID" json:"author"`
+	Game         Game           `gorm:"foreignKey:GameID;references:AppID" json:"game"`
+	CreatedAt    time.Time      `json:"-"`
+	UpdatedAt    time.Time      `json:"-"`
+	DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
 }
