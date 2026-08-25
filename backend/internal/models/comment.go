95fb434903fd7e256532e03dc43cc296c25964a2
hrouchy <hrouchy@f6r7s5.paris.42.school>
2026-08-25 17:15:18 +0200

    perfectionnement du chat avec maj auto des listes

--- backend/internal/models/comment.go
+++ backend/internal/models/comment.go
@@ -1,25 +1,33 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-type Comment struct {
-	gorm.Model	
+type (
+	Comment struct {
+		ID           uuid.UUID      `gorm:"primary_key;type:uuid;default:gen_random_uuid()" json:"id"`
+		Comment      string         `gorm:"type:text" json:"comment"`
+		CommentTitle string         `gorm:"type:text" json:"comment_title"`
+		Rating       float64        `gorm:"type:float" json:"rating"`
+		Likes        int            `gorm:"type:int; default:0" json:"likes"`
+		Dislikes     int            `gorm:"type:int; default:0" json:"dislikes"`
+		AuthorID     uuid.UUID      `gorm:"type:uuid; uniqueIndex:idx_author_game;constraint:onDelete:CASCADE" json:"-"`
+		GameID       uint64         `gorm:"type:bigint; uniqueIndex:idx_author_game" json:"game_id"`
+		Author       User           `gorm:"foreignKey:AuthorID;references:ID" json:"author"`
+		Game         Game           `gorm:"foreignKey:GameID;references:AppID" json:"-"`
+		CreatedAt    time.Time      `json:"-"`
+		UpdatedAt    time.Time      `json:"-"`
+		DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
+	}
 
-	Comment      string    `gorm:"type:text" json:"comment"`
-	CommentTitle string    `json:"comment_title"`
-	Rating       float64   `json:"rating"`
-	Likes        int       `gorm:"default:0" json:"likes"`
-	Dislikes     int       `gorm:"default:0" json:"dislikes"`
-	UserID       uuid.UUID `json:"user_id"`
-	GameID       uint64    `json:"game_id"`
-	Author       string	   `json:"author"`
-	User         User      `gorm:"foreignKey:UserID;references:ID"`
-	Title1		 string	   `json:"title_1"`
-	Title2		 string	   `json:"title_2"`
-	Game         Game      `gorm:"foreignKey:GameID;references:AppID"`
-	ProfilePic   string    `gorm:"typevarchar(100)" json:"profile_picture"`
-	
-}
+	CommentRequest struct {
+		GameID       uint64  `json:"game_id" binding:"required"`
+		Comment      string  `json:"comment" binding:"required"`
+		CommentTitle string  `json:"comment_title" binding:"required"`
+		Rating       float64 `json:"rating" binding:"required"`
+	}
+)
